4c252c3a2995d759421cb9d743bda3396c8fd807
lzx <lzx@lzx.com>
2018-09-28 15:14:34 +0800

    商品表单修改及三级联动

--- controllers/BrandController.php
+++ controllers/BrandController.php
@@ -1,38 +1,38 @@
 <?php
 namespace controllers;
 
-use models\Vip;
+use models\Brand;
 
-class VipController{
+class BrandController{
     // 列表页
     public function index()
     {
-        $model = new Vip;
+        $model = new Brand;
         $data = $model->findAll();
-        view('vip/index', $data);
+        view('brand/index', $data);
     }
 
     // 显示添加的表单
     public function create()
     {
-        view('vip/create');
+        view('brand/create');
     }
 
     // 处理添加表单
     public function insert()
     {
-        $model = new Vip;
+        $model = new Brand;
         $model->fill($_POST);
         $model->insert();
-        redirect('/vip/index');
+        redirect('/brand/index');
     }
 
     // 显示修改的表单
     public function edit()
     {
-        $model = new Vip;
-        $data = $model->findOne($_GET['id']);
-        view('vip/edit', [
+        $model = new Brand;
+        $data=$model->findOne($_GET['id']);
+        view('brand/edit', [
             'data' => $data,    
         ]);
     }
@@ -40,17 +40,17 @@ class VipController{
     // 修改表单的方法
     public function update()
     {
-        $model = new Vip;
+        $model = new Brand;
         $model->fill($_POST);
         $model->update($_GET['id']);
-        redirect('/vip/index');
+        redirect('/brand/index');
     }
 
     // 删除
     public function delete()
     {
-        $model = new Vip;
+        $model = new Brand;
         $model->delete($_GET['id']);
-        redirect('/vip/index');
+        redirect('/brand/index');
     }
 }
